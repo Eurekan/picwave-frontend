0e8d8c7d1682725e6b7459978809d93733e41e14
Eurekan <2455096251@qq.com>
2025-03-07 21:25:56 +0800

    :tada: 初始化项目

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = "欢迎来到图潮，海量高清素材~";
+</script>
+
+<style scoped>
+#homePage {
+}
+</style>
